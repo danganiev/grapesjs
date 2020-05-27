c0539923595076ad4f772888b5b39ccdc9fa1fbc
Dan Ganiev <daniil.ganiev@gmail.com>
2020-05-27 16:40:55 +0300

    Test

--- src/dom_components/view/ComponentTextView.js
+++ src/dom_components/view/ComponentTextView.js
@@ -5,7 +5,7 @@ const compProt = ComponentView.prototype;
 
 export default ComponentView.extend({
   events: {
-    click: 'onActive',
+    // click: 'onActive',
     dblclick: 'onActive',
     input: 'onInput'
   },
